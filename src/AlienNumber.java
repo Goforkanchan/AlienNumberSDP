7df7fb5ab19edd3cb20dc4c450bfbd07123fccf7
Goforkanchan <kanchanmahajan67@gmail.com>
2021-08-23 21:12:04 +0530

    Modified the code to accept any base for alien number system. This made code more generic so that this would work for any base.

--- src/AlienNumber.java
+++ src/AlienNumber.java
@@ -1,36 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class AlienNumber {
     //ArrayList of a number to store the value of each special char.
     static ArrayList<Character> mapChar = new ArrayList<>();
-    public static void main(String args[]) {
+    public static void main(String args[]) throws IOException {
         //String input
-        String str1 = "!@^*!";
-        String str2 = "!!!";
-        String str3 = "^!@*!";
-        String str4 = "@@@!!!!";
-        String str5 = "!!!@@";
-        mapChar.add('!');
-        mapChar.add('@');
-        mapChar.add('^');
-        mapChar.add('&');
-        mapChar.add('*');
-        succ_alien(str1);
-        succ_alien(str2);
-        succ_alien(str3);
-        succ_alien(str4);
-        succ_alien(str5);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter the base of alien number system:");
+        int base = Integer.parseInt(br.readLine());
+        System.out.println("Enter the characters in the alien number system:");
+        String chars[] = br.readLine().split(" ");
+        for (String ch : chars)
+            mapChar.add(ch.charAt(0));
+        String str = br.readLine();
+        succ_alien(str, base);
     }
 
-    public static void succ_alien(String str1) {
+    public static void succ_alien(String str1, Integer base) {
         ArrayList<Integer> number = new ArrayList<>();
         for (int i = 0; i < str1.length(); i++) {
             number.add(mapChar.indexOf(str1.charAt(i)));
         }
-        System.out.println(number);
-        int decimal = getDecimalVal(number);
-        String res = increasedNum(decimal);
-
+        int decimal = getDecimalVal(number, base);
+        String res = increasedNum(decimal, base);
         //Considering leading zeros
         int j = 0;
         for (int i = 0; i < number.size(); i++) {
@@ -42,7 +37,6 @@ public class AlienNumber {
         }
         if (j == number.size())
             res = res.substring(1);
-        System.out.println(res);
         String encodedStr = encode(res);
         System.out.println(encodedStr);
     }
@@ -56,23 +50,23 @@ public class AlienNumber {
     }
 
     //Returns result string
-    public static String increasedNum(int decimal) {
+    public static String increasedNum(int decimal, int base) {
         String res = "";
         //Increase the decimal value
         decimal = decimal + 1;
         while (decimal > 0) {
-            int rem = decimal % 5;
+            int rem = decimal % base;
             res = rem + res;
-            decimal = decimal / 5;
+            decimal = decimal / base;
         }
         return res;
     }
 
     //Decimal equivalent of encoded string
-    public static int getDecimalVal(ArrayList<Integer> num) {
+    public static int getDecimalVal(ArrayList<Integer> num, int base) {
         int decimal = 0;
         for (int i = 0; i < num.size(); i++) {
-            decimal += Math.pow(5, num.size() - i - 1)*num.get(i);
+            decimal += Math.pow(base, num.size() - i - 1)*num.get(i);
         }
         return decimal;
     }
