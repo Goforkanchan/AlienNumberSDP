935f7468f9528902f162b9bb8d047b39cd93d927
Goforkanchan <kanchanmahajan67@gmail.com>
2021-08-23 13:59:15 +0530

    Added proper comments.

--- src/AlienNumber.java
+++ src/AlienNumber.java
@@ -23,15 +23,18 @@ public class AlienNumber {
         for (int i = 0; i < num.size(); i++) {
             decimal += Math.pow(5, num.size() - i - 1)*num.get(i);
         }
-        System.out.println(decimal);
+//        System.out.println(decimal);
+        //Increase the decimal value
         decimal = decimal + 1;
         while (decimal > 0) {
             int rem = decimal % 5;
             res = rem+res;
             decimal = decimal/5;
         }
+        //Adding zero at the start
         if (num.get(0) == 0)
             res = '0'+res;
+        //Encoding the increased number
         String encodedStr = "";
         for (int i = 0; i < res.length(); i++)
             encodedStr += arr.get(res.charAt(i) - 48);
